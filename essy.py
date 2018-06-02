410b721a4ceaf97f5be937ea38bdbc42299fcf48
vivaentertainment <estherwanjirumaina21@gmail.com>
2018-06-02 10:25:24 +0300

    first commit

---
+++ essy.py
@@ -0,0 +1,18 @@
+liST1=[1,2,3,4]
+print(list1)
+
+List2=["faith","essy","loise"]
+print(list2)
+
+list3=["essy","faith",7,2]
+print(list3)
+
+
+tuple=("nairobi","mombasa",78,55)
+
+first_list=[1,2,"esst","faith",4,8]
+first_set=(first_list)
+print(first_set)
+
+dict{"name":"essy","age":22,"adress":"nairobi"}
+print (dict)
